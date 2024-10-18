2ce36af207f78340ea2bed44b7a12dcd93a59dc3
Rezki Triandy <rtriandy@gmail.com>
2024-10-18 08:57:33 +0700

    chore: update handle error

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -4,6 +4,9 @@ import { IPost } from '@/utils/types'
 
 const HomePage = async () => {
   const data = await fetch(ENDPOINTS.posts)
+  if (!data.ok) {
+    throw new Error('Failed to get posts')
+  }
   const posts: IPost[] = await data.json()
 
   return <NewsFeed posts={posts} />
